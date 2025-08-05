e59b84e88f84aedceadadf3a4a977817e8832853
dev-multipla <dev@multipla.tec.br>
2025-08-05 13:13:51 -0300

    Remove duplicated CentroCusto model

--- financeiro/models.py
+++ financeiro/models.py
@@ -21,44 +21,9 @@ class ContaFinanceira(models.Model):
     def delete(self, *args, **kwargs):
         """Soft-delete: apenas marca como inativo"""
         self.is_active = False
-        self.save() 
-
-class CentroCusto(models.Model):
-    id = models.AutoField(primary_key=True)
-    descricao = models.CharField(max_length=100, blank=False, null=False)
-    mascara_centro_custo = models.CharField(max_length=3, unique=True)
-    is_active = models.BooleanField(default=True)
-    empresa = models.ForeignKey(
-        Empresa,
-        on_delete=models.PROTECT,
-        db_index=True,
-        db_constraint=False,   
-    )
-
-    class Meta:
-        # agora cada empresa pode ter sua própria sequência de máscaras
-        unique_together = (('empresa', 'mascara_conta'),)
-        verbose_name = 'Conta Financeira'
-        verbose_name_plural = 'Contas Financeiras'
-
-    def delete(self, *args, **kwargs):
-        """Soft-delete: apenas marca como inativo"""
-        self.is_active = False
         self.save()
 
-    def save(self, *args, **kwargs):
-        if not self.mascara_centro_custo:
-            # Gera a máscara de forma sequencial automaticamente
-            ultimo_centro = CentroCusto.objects.order_by('id').last()
-            if ultimo_centro:
-                ultimo_codigo = int(ultimo_centro.mascara_centro_custo)
-                self.mascara_centro_custo = f'{ultimo_codigo + 1:03d}'
-            else:
-                self.mascara_centro_custo = '001'
-        super(CentroCusto, self).save(*args, **kwargs)
 
-    def __str__(self):
-        return f"{self.descricao} ({self.mascara_centro_custo})"
 class CentroCusto(models.Model):
     id = models.AutoField(primary_key=True)
     descricao = models.CharField(max_length=100, blank=False, null=False)
@@ -76,6 +41,11 @@ class CentroCusto(models.Model):
         verbose_name = 'Centro de Custo'
         verbose_name_plural = 'Centros de Custo'
 
+    def delete(self, *args, **kwargs):
+        """Soft-delete: apenas marca como inativo"""
+        self.is_active = False
+        self.save()
+
     def save(self, *args, **kwargs):
         # Gera sequência por tenant
         tenant = get_current_tenant()
@@ -88,3 +58,6 @@ class CentroCusto(models.Model):
             else:
                 self.mascara_centro_custo = '001'
         super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.descricao} ({self.mascara_centro_custo})"
